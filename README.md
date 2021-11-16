77586bf8a0c6265a64260f1e5b2f1a5d3f612937
Liquan (Max) Gu <maxlg@chromium.org>
2021-11-16 17:25:06 -0500

    adding html

---
+++ README.md
@@ -0,0 +1,19 @@
+# Serving static files in Node.js sample for Google App Engine
+
+This sample demonstrates serving static files in a Node.js app for [Google App Engine](https://cloud.google.com/appengine) Node.js [standard environment](https://cloud.google.com/appengine/docs/standard/nodejs)
+and [flexible environment](https://cloud.google.com/appengine/docs/flexible/nodejs)
+
+## Running locally
+
+Refer to the [appengine/README.md](../README.md) file for instructions on
+running and deploying.
+
+
+## Login
+gcloud auth login
+
+## Set current project
+gcloud config set project maxlg-big-query-project
+
+## deploy web app
+gcloud app deploy app.standard.yaml
---
+++ app.js
@@ -0,0 +1,42 @@
+// Copyright 2016 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// [START gae_flex_node_static_files]
+'use strict';
+
+const express = require('express');
+const app = express();
+
+// app.set('view engine', 'pug');
+
+// Use the built-in express middleware for serving static files from './public'
+app.use('/static', express.static('public'));
+app.use(express.static(__dirname + '/view'));
+
+// app.get('/', (req, res) => {
+//   res.setHeader('Supports-Loading-Mode', 'fenced-frame');
+//   res.render('index');
+// });
+
+app.get('/',function(req,res){
+  res.sendFile('/index.html');
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_flex_node_static_files]
